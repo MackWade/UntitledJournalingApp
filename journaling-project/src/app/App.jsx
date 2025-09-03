97374f0716a075d5dc0107d9d177fa67cc07d1ff
Mackenzie Wade <kinkinwade@gmail.com>
2025-09-03 12:56:54 -0500

    feat: sidenavbar created

--- journaling-project/src/app/App.jsx
+++ journaling-project/src/app/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import SideNavBar from "../components /SideNavBar.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-          hello world
+         <SideNavBar/>
       </div>
     </>
   )
---
+++ journaling-project/src/components /SideNavBar.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+export default function SideNavBar() {
+    const [open, setOpen] = React.useState(true);
+
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
+
+    const DrawerList = (
+        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+            <List>
+                {['journal App', 'Home','journal'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+        </Box>
+    );
+
+    return (
+        <div>
+            <Button onClick={toggleDrawer(true)}>Open drawer</Button>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                {DrawerList}
+            </Drawer>
+        </div>
+    );
+}

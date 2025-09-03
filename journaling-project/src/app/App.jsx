4bf401a881cddc407499cc88f863e01366b1e8ef
Mackenzie Wade <kinkinwade@gmail.com>
2025-09-03 01:20:10 -0500

    init: initial project setup

---
+++ journaling-project/src/app/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          hello world
+      </div>
+    </>
+  )
+}
+
+export default App
